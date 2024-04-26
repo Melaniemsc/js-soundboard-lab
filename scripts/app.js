d113aa1dd7bed746cf8d5c38b0fbd606ac7d4030
Melaniemsc <melsan1706@gmail.com>
2024-04-26 15:31:46 +0100

    initial

---
+++ scripts/app.js
@@ -0,0 +1,39 @@
+
+/*-------------------------------- Constants --------------------------------*/
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+
+
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+
+JS Soundboard Task
+=====
+You will be given starter code containing all the `sounds` required for this lab.
+You will also be provided HTML and CSS for this lab. Feel free to update this however you like!
+## Introduction
+Your task is to make a Daft Punk soundboard using HTML, CSS, JavaScript and the HTML5 audio tag!
+## Exercise
+#### Requirements
+ Each button should play the correct sound when clicked
+ When you click **Randomize** your app should play a random daft punk sound.
+ Visually pleasing design! :)
+Level Up :rocket::
+ When you click **Randomize**, it should visually indicate the sound that’s being played.
+ When your mouse cursor hovers away from the **Randomize** button, the visual indicator should be removed.
+ Extra challenge: When you
+#### Starter code
+You have been given a folder with all the sound files you will need to complete the exercise. (edited)
