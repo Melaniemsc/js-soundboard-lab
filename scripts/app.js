8babc62c67bfe86fe1d9a4089a713643ddc0bad5
Melaniemsc <melsan1706@gmail.com>
2024-04-27 11:49:28 +0100

    initial

--- scripts/app.js
+++ scripts/app.js
@@ -1,26 +1,14 @@
+const workItButton = document.querySelector('#work_it') 
+const audioPlayer = document.querySelector('#audio')
 
-/*-------------------------------- Constants --------------------------------*/
+function play() {
+    console.log('hello');
+    audioPlayer.src = './sound/work_it.wav'
+    audioPlayer.play()
+}
+workItButton.addEventListener('click',play)
 
-
-
-/*-------------------------------- Variables --------------------------------*/
-
-
-
-/*------------------------ Cached Element References ------------------------*/
-
-
-
-/*-------------------------------- Functions --------------------------------*/
-
-
-
-/*----------------------------- Event Listeners -----------------------------*/
-
-
-
-
-JS Soundboard Task
+/*JS Soundboard Task
 =====
 You will be given starter code containing all the `sounds` required for this lab.
 You will also be provided HTML and CSS for this lab. Feel free to update this however you like!
@@ -34,6 +22,6 @@ Your task is to make a Daft Punk soundboard using HTML, CSS, JavaScript and the 
 Level Up :rocket::
  When you click **Randomize**, it should visually indicate the sound that’s being played.
  When your mouse cursor hovers away from the **Randomize** button, the visual indicator should be removed.
- Extra challenge: When you
+ Extra challenge (super level up): Add another button to your soundboard called Play  Song*. This button should play each sound in the correct order until its played all of them “work it harder make it better do it faster makes us stronger.. etc”. Looping won’t work for this as you need time between each sound. Have a look at https://developer.mozilla.org/en-US/docs/Web/API/setInterval for this one.
 #### Starter code
-You have been given a folder with all the sound files you will need to complete the exercise. (edited)
+You have been given a folder with all the sound files you will need to complete the exercise*/
